df881bf28315c964b459846ad37417489d4dd060
mariamdahibi <dahibi.mariam@ensi.ma>
2026-04-17 20:41:41 +0200

    first commit

---
+++ recrutement_intelligent_app/src/main/java/com/recrutement/recrutement_intelligent_app/RecrutementIntelligentAppApplication.java
@@ -0,0 +1,13 @@
+package com.recrutement.recrutement_intelligent_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecrutementIntelligentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecrutementIntelligentAppApplication.class, args);
+	}
+
+}
---
+++ recrutement_intelligent_app/src/test/java/com/recrutement/recrutement_intelligent_app/RecrutementIntelligentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recrutement.recrutement_intelligent_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecrutementIntelligentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
