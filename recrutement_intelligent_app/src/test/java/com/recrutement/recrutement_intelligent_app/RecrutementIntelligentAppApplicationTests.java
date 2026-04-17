c0cbe209fb89218d485c30e14895674b4ca1e852
mariamdahibi <dahibi.mariam@ensi.ma>
2026-04-17 22:44:18 +0200

    fix issues

--- recrutement_intelligent_app/src/test/java/com/recrutement/recrutement_intelligent_app/RecrutementIntelligentAppApplicationTests.java
+++ recrutement_intelligent_app/src/test/java/com/recrutement/recrutement_intelligent_app/RecrutementIntelligentAppApplicationTests.java
@@ -2,12 +2,15 @@ package com.recrutement.recrutement_intelligent_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RecrutementIntelligentAppApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+
 
 }
